99ee1e488e58351314f59e72c9fab5d790a33242
pbascunana <pablobascunanasaiz@gmail.com>
2021-08-29 16:18:51 +0200

    Add mongo db dependencies and configuration

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from config import logger
+from config.mongo import db
 from config.swagger import SWAGGER_CONFIG
 from flask import Flask
 from flask_restful import Api
@@ -16,6 +17,8 @@ app = Flask(__name__)
 logging.info('The server mode is: {}'.format(app.config['ENV']))
 logging.info('The app is in debug mode: {}'.format(os.getenv('DEBUG')))
 
+app.config["MONGODB_HOST"] = os.getenv('DATABASE')
+app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config['SWAGGER'] = SWAGGER_CONFIG
 
 swagger = Swagger(app)
@@ -25,4 +28,5 @@ api = Api(app)
 logging.info('The version number is: {}'.format(os.getenv('VERSION_NUMBER')))
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=os.getenv('DEBUG'))
