0c6fc7e04aeaa6cefaa174c37425671846b3e3bc
pbascunana <pablobascunanasaiz@gmail.com>
2021-09-02 15:26:15 +0200

    Add security in location services. Fix JWT callbacks

--- resources/location.py
+++ resources/location.py
@@ -1,5 +1,6 @@
 from flasgger import swag_from
 from flask import request
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from schemas.location import LocationSchema, MarkerSchema
 from models.location import Location as LocationModel
@@ -15,12 +16,14 @@ SWAGGER_STATUS_PATH = SWAGGER_PATH + 'location/'
 
 class Location(Resource):
     @classmethod
+    @jwt_required()
     @swag_from(SWAGGER_STATUS_PATH + 'location.yml')
     def get(cls, user_uuid):
         markers = LocationModel.get_locations_by_user_id(user_uuid)
         return created(location_schema.dump(markers))
 
     @classmethod
+    @jwt_required()
     @swag_from(SWAGGER_STATUS_PATH + 'location.yml')
     def post(cls, user_uuid):
         location = location_schema.load(request.get_json(), partial=True)
@@ -28,6 +31,7 @@ class Location(Resource):
         return created(location_schema.dump(location))
 
     @classmethod
+    @jwt_required()
     @swag_from(SWAGGER_STATUS_PATH + 'location.yml')
     def delete(cls, user_uuid):
         marker = marker_schema.load(request.get_json(), partial=True)
--- utils/jwt.py
+++ utils/jwt.py
@@ -4,9 +4,8 @@ from utils.responses import unauthorized
 
 
 def create_token_callbacks(jwt):
-
     @jwt.expired_token_loader
-    def expired_token_callback():
+    def expired_token_callback(jwt_header, jwt_payload):
         logging.error("The token has expired")
         return unauthorized({
             'description': 'The token has expired.',
@@ -14,23 +13,23 @@ def create_token_callbacks(jwt):
         })
 
     @jwt.invalid_token_loader
-    def invalid_token_callback():
-        logging.error("Signature verification failed")
+    def invalid_token_callback(reason):
+        logging.error("Signature verification failed: {}".format(reason))
         return unauthorized({
             'description': 'Signature verification failed.',
             'error': 'token_invalid'
         })
 
     @jwt.unauthorized_loader
-    def unauthorized_callback():
-        logging.error("Request does not contain an access token")
+    def unauthorized_callback(reason):
+        logging.error("Request does not contain an access token: {}".format(reason))
         return unauthorized({
             'description': 'Request does not contain an access token.',
             'error': 'authorization_required'
         })
 
     @jwt.needs_fresh_token_loader
-    def needs_fresh_token_callback():
+    def needs_fresh_token_callback(jwt_header, jwt_payload):
         logging.error("The token is not fresh")
         return unauthorized({
             'description': 'The token is not fresh.',
